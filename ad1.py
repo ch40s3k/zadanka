81a077edfd593db131ce84262048d58aa703dd7c
Hubert Sajnaj <hubert.sajnaj@allegro.pl>
2015-04-20 19:58:42 +0200

    adding if

--- ad1.py
+++ ad1.py
@@ -11,10 +11,12 @@ def _capitalize(words):
 	'''
 	Return First Letter Capitalized 
 	'''
-	return ''.join(word[0].upper() + word[1:] for word in re.split('(\s+)', words))
+	return ''.join(word[0].upper() + word[1:] for word in re.split('(\s+)', words) if len(word)>0)
 
 
 
 words = "Taki tam tesCIOR \nco sprawdza czy dziAla"
 print _capitalize(words)
 
+empty = "   "
+print _capitalize(empty)
